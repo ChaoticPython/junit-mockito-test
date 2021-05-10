0cb6b0c7578ddef5c00611d182deb8af51159ce4
Julio Efrain Pacheco Sanginez <efrain.pacheco@ibm.com>
2021-05-10 12:31:00 -0500

    Se creo el proyecto para realizar y probar nuestras pruebas unitarias con JUnit y Mockito

---
+++ src/main/java/org/test/app/JunitMockitoTestApplication.java
@@ -0,0 +1,13 @@
+package org.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMockitoTestApplication.class, args);
+	}
+
+}
