b4f12d89e1d6aaa40d8a2802a4f70095b1ab25ed
Julio Efrain Pacheco Sanginez <efrain.pacheco@ibm.com>
2021-05-11 11:54:54 -0500

    Se creo la clase de servicio y además se implemento su interfaz

--- src/main/java/org/test/app/repository/ICuentaRepository.java
+++ src/main/java/org/test/app/repository/ICuentaRepository.java
@@ -1,5 +1,15 @@
 package org.test.app.repository;
 
+import org.test.app.model.Cuenta;
+
+import java.util.List;
+
 public interface ICuentaRepository {
 
+  List<Cuenta> findAll();
+  
+  Cuenta findById(Long id);
+  
+  void update(Cuenta cuenta);
+  
 }
---
+++ src/main/java/org/test/app/service/CuentaServiceImpl.java
@@ -0,0 +1,54 @@
+package org.test.app.service;
+
+import org.test.app.model.Banco;
+import org.test.app.model.Cuenta;
+import org.test.app.repository.IBancoRepository;
+import org.test.app.repository.ICuentaRepository;
+
+import java.math.BigDecimal;
+
+public class CuentaServiceImpl implements ICuentaService {
+
+  private ICuentaRepository cuentaRepository;
+  
+  private IBancoRepository bancoRepository;
+
+  public CuentaServiceImpl(ICuentaRepository cuentaRespository, IBancoRepository bancoRepository) {
+    this.cuentaRepository = cuentaRespository;
+    this.bancoRepository = bancoRepository;
+  }
+  
+  @Override
+  public Cuenta findById(Long id) {
+    return cuentaRepository.findById(id);
+  }
+
+  @Override
+  public int revisarTotalTransferencias(Long bancoId) {
+    Banco banco = bancoRepository.findById(bancoId);
+    return banco.getTotaltransferencias();
+  }
+
+  @Override
+  public BigDecimal revisarSaldo(Long cuentaId) {
+    Cuenta cuenta = cuentaRepository.findById(cuentaId);
+    return cuenta.getSaldo();
+  }
+
+  @Override
+  public void transferir(Long numeroCuentaOrigen, Long numeroCuentaDestino, BigDecimal monto) {
+    Banco banco = bancoRepository.findById(1L);
+    int totalTransferencias = banco.getTotaltransferencias();
+    banco.setTotaltransferencias(++totalTransferencias);
+    bancoRepository.update(banco);
+    
+    Cuenta cuentaOrigen = cuentaRepository.findById(numeroCuentaOrigen);
+    cuentaOrigen.debito(monto);
+    cuentaRepository.update(cuentaOrigen);
+    
+    Cuenta cuentaDestino = cuentaRepository.findById(numeroCuentaDestino);
+    cuentaDestino.credito(monto);
+    cuentaRepository.update(cuentaDestino);
+  }
+
+}
