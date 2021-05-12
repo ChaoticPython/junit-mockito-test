520c2820523ccaffc169e67ef1e8090e6c5fddee
Julio Efrain Pacheco Sanginez <efrain.pacheco@ibm.com>
2021-05-12 18:52:32 -0500

    Se modificaron un poco los repositorios, aún quedan pendientes

--- src/main/java/org/test/app/repository/IBancoRepository.java
+++ src/main/java/org/test/app/repository/IBancoRepository.java
@@ -3,7 +3,7 @@ package org.test.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.test.app.model.Banco;
 
-import java.util.List;
+//import java.util.List;
 
 public interface IBancoRepository extends JpaRepository<Banco, Long> {
 
--- src/main/java/org/test/app/repository/ICuentaRepository.java
+++ src/main/java/org/test/app/repository/ICuentaRepository.java
@@ -1,9 +1,11 @@
 package org.test.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.test.app.model.Cuenta;
 
-import java.util.List;
+//import java.util.List;
+import java.util.Optional;
 
 public interface ICuentaRepository extends JpaRepository<Cuenta, Long> {
 
@@ -13,4 +15,7 @@ public interface ICuentaRepository extends JpaRepository<Cuenta, Long> {
   //
   // void update(Cuenta cuenta);
 
+  @Query("select c from Cuenta c where c.nombrePersona = ?1")
+  Optional<Cuenta> findByNombrePersona(String nombrePersona);
+  
 }
